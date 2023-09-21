e3b0dd3ff284c1b7cce59c56e3e5022ba020d772
Santiago Bojorquez Leyva <132846139+Sonicus-dev@users.noreply.github.com>
2023-09-20 20:31:40 -0700

    Primer push
    Creación del proyecto

---
+++ Staem_Presentaciones/src/main/java/com/mycompany/staem_presentacion/Staem_Presentaciones.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.staem_presentacion;
+
+/**
+ *
+ * @author santi
+ */
+public class Staem_Presentaciones {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
