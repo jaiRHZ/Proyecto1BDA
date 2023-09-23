9cca5a20c08287ef4ae77d937592c26e7d48eeb4
Juliomst <juliosotomayor121@gmail.com>
2023-09-22 20:18:38 -0700

    BD Actualizada
    Actualización de la base de datos

--- Proyecto1_Equipo3.sql
+++ Proyecto1_Equipo3.sql
@@ -3,16 +3,16 @@ USE tiendaVideojuegos;
 
 CREATE TABLE Compradores (
 id INT PRIMARY KEY AUTO_INCREMENT,
-email VARCHAR(50),
-usuario VARCHAR(25) UNIQUE,
-nombres VARCHAR(20),
-aPaterno VARCHAR(20),
+email VARCHAR(50) NOT NULL,
+usuario VARCHAR(25) UNIQUE NOT NULL,
+nombres VARCHAR(20) NOT NULL,
+aPaterno VARCHAR(20) NOT NULL,
 aMaterno VARCHAR(20),
-calle VARCHAR(20),
-numero INT,
-codigoPostal INT,
-idCredencial INT,
-contrasena VARCHAR(25)
+calle VARCHAR(20) NOT NULL,
+numero INT NOT NULL,
+codigoPostal INT NOT NULL,
+idCredencial INT NOT NULL,
+contrasena VARCHAR(25) NOT NULL
 );
 
 CREATE TABLE Compras(
@@ -24,15 +24,15 @@ FOREIGN KEY (idComprador) REFERENCES Compradores(id)
 
 CREATE TABLE Videojuegos(
 idJuego INT PRIMARY KEY AUTO_INCREMENT,
-estudio VARCHAR(20),
-clasificacion VARCHAR(20),
-titulo VARCHAR(50),
-genero VARCHAR(20)
+estudio VARCHAR(20) NOT NULL,
+clasificacion VARCHAR(20) NOT NULL,
+titulo VARCHAR(50) NOT NULL,
+genero VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE Copias(
 idCopia INT PRIMARY KEY AUTO_INCREMENT,
-precio INT,
+precio INT NOT NULL,
 idVideojuego INT,
 FOREIGN KEY (idVideojuego) REFERENCES Videojuegos(idJuego)
 );
@@ -41,9 +41,7 @@ CREATE TABLE compraCopias(
 idCompraCopias INT PRIMARY KEY AUTO_INCREMENT,
 idCompras INT,
 idCopia INT,
-cantidad INT,
+cantidad INT NOT NULL,
 FOREIGN KEY (idCompras) REFERENCES Compras(idCompras),
 FOREIGN KEY (idCopia) REFERENCES Copias(idCopia)
 );
-
-DROP DATABASE tiendaVideojuegos;
