35a4dd8579fbc432dda0b227039f5cca3d84a68b
jaiRHZ <133193972+jaiRHZ@users.noreply.github.com>
2023-09-22 21:18:27 -0700

    Creacion de Clases DAO
    Se ha creado las clases DAO restantes

---
+++ Staem_Persistencia/src/main/java/implementaciones/CompradoresDAO.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package implementaciones;
+
+import dominio.Comprador;
+import interfacesDAO.ICompradoresDAO;
+
+/**
+ *
+ * @author Jairo G. Rodriguez Hernandez 00000213248
+ */
+public class CompradoresDAO implements ICompradoresDAO{
+
+    @Override
+    public Comprador agregarComprador(Comprador comprador) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void eliminarComprador(Comprador comprador) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Comprador actualizarComprador(Comprador comprador) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Comprador consultarComprador(Comprador comprador) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ Staem_Persistencia/src/main/java/implementaciones/CopiasDAO.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package implementaciones;
+
+import dominio.Copia;
+import interfacesDAO.ICopiasDAO;
+
+/**
+ *
+ * @author Jairo G. Rodriguez Hernandez 00000213248
+ */
+public class CopiasDAO implements ICopiasDAO{
+
+    @Override
+    public Copia agregarCopia(Copia copia) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void eliminarCopia(Copia copia) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Copia actualizarCopia(Copia copia) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Copia consultarCopia(Copia copia) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
